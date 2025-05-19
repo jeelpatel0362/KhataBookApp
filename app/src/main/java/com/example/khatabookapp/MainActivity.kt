adb565bd83c3f57358a85a1fdb5184f660e148e9
jeelpatel0362 <jeelpatel0362@gmail.com>
2025-05-19 15:36:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/khatabookapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.khatabookapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.khatabookapp.fragments.HomeFragment
+import com.example.khatabookapp.fragments.UserFragments
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottom_navigation)
+
+        loadFragment(HomeFragment())
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+
+                R.id.nav_user -> {
+                    loadFragment(UserFragments())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
